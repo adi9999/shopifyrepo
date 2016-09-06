8874166e438c0f18ac86565ceb1096f4980e953a
adi9999 <adeel.abid.sofizar@gmail.com>
2016-09-06 12:32:01 +0500

    Update shopify.js

--- routes/shopify.js
+++ routes/shopify.js
@@ -52,7 +52,7 @@ shopifyRouter.get('/', function(req, res, next) {
     
     if(req.query.shop != undefined){
         //shopifyRouter.shop = shop;
-        console.log(this.shopifyRouter.shop);
+        console.log(global.shopifyRouter.shop);
         console.log(req.query.shop);
     }
     res.send("Goodd");
