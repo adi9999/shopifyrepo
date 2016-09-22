b0e87237aec61efd0781f84554a68bd67dfb5bba
adi9999 <adeel.abid.sofizar@gmail.com>
2016-09-22 16:30:55 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   ////Check if App installed ///////
-  /// if req.query.shop exist in db than res.redirect('https://'+shopifyRouter.shop+'/admin/apps'); 
+  /// if req.query.shop exist in db than  res.render('index', { title: 'Express' });
   /// else res.redirect('https://'+shopifyRouter.shop+'/shopify');
   /// and comment below line res.render('index', { title: 'Express' });
   //////////////////////////////////
